2e9a458bc67fa055d0df641a019a57c94dd0ae60
Ionut Alexandru Muresan <alexandru.muresan@fortech.ro>
2022-01-28 10:09:36 +0200

    implemented Chart feature for Dashboard

--- src/pages/Dashboard.vue
+++ src/pages/Dashboard.vue
@@ -1,7 +1,53 @@
-<template><h2>Dashboard</h2></template>
+<template>
+  <h2>Daily Sales</h2>
+
+  <div id="chart"></div>
+</template>
 
 <script lang="ts">
+import { onMounted } from "vue";
+import axios from "axios";
+import * as c3 from "c3";
 export default {
   name: "Dashboard",
+  setup() {
+    onMounted(async () => {
+      try {
+        const chart = c3.generate({
+          bindto: "#chart",
+          data: {
+            x: "x",
+            columns: [["x"], ["sales"]],
+            types: {
+              Sales: "bar",
+            },
+          },
+          axis: {
+            x: {
+              type: "timeseries",
+              tick: {
+                format: "%Y-%m%d",
+              },
+            },
+          },
+        });
+
+        const { data } = await axios.get("chart");
+
+        console.log(data);
+
+        chart.load({
+          columns: [
+            ["x", ...data.map((record: { date: any; }) => record.date)],
+            ["sales", ...data.map((record: { sum: any; }) => record.sum)],
+          ],
+        });
+      } catch (e) {
+        console.log(e);
+      }
+    });
+
+    return {};
+  },
 };
 </script>
